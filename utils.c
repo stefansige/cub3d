65c9c44bc4bfdc07794076c63cf99426b04d51cb
gbarone42 <badph3@gmail.com>
2024-03-29 14:52:55 +0100

    utils

--- utils.c
+++ utils.c
@@ -60,3 +60,80 @@ void	*ft_realloc(void *ptr, size_t size)
 	ft_memcpy(new_ptr, ptr, size);
 	return (new_ptr);
 }
+
+char		*ft_strdup(const char *s)
+{
+	char	*str;
+	int		i;
+
+	i = 0;
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	str = (char*)malloc(sizeof(char) * ft_strlen(s) + 1);
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	while (s[i] != '\0')
+	{
+		str[i] = s[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+
+
+//ATOI
+int		ft_isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	else
+		return (0);
+}
+
+static const char	*ft_remove(const char *str)
+{
+	while (*str == ' ' || *str == '\t' || *str == '\n' || *str == '\v'
+			|| *str == '\f' || *str == '\r')
+		str++;
+	return (str);
+}
+
+static void			ft_assign(int *i, int *res, int *sign)
+{
+	*i = 0;
+	*res = 0;
+	*sign = 1;
+}
+
+int					ft_atoi(const char *str)
+{
+	int		res;
+	int		sign;
+	int		i;
+
+	ft_assign(&i, &res, &sign);
+	if (str == NULL)
+		return (0);
+	str = ft_remove(str);
+	if (str[i] == '-' || str[i] == '+')
+	{
+		sign = (str[i] == '-') ? -1 : 1;
+		i++;
+	}
+	while (str[i] != '\0')
+	{
+		if (ft_isdigit(str[i]) == 0)
+			return (res * sign);
+		res = res * 10 + str[i] - '0';
+		i++;
+	}
+	return (sign * res);
+}
+
+//ATOI
