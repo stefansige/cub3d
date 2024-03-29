9a6f9c413c726fcd462dd1efb50ad0552032a3a5
gbarone42 <badph3@gmail.com>
2024-03-29 14:28:09 +0100

    utils

--- utils.c
+++ utils.c
@@ -30,3 +30,33 @@ void ft_putstr(char *str)
 	while (str && str[i])
 		write(1, &str[i++], 1);
 }
+
+void		*ft_memcpy(void *s1, const void *s2, size_t n)
+{
+	size_t	i;
+	char	*dest;
+	char	*src;
+
+	dest = (char*)s1;
+	src = (char*)s2;
+	i = 0;
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
+
+void	*ft_realloc(void *ptr, size_t size)
+{
+	void	*new_ptr;
+
+	if (ptr == NULL)
+		return (malloc(size));
+	if (!size)
+		return (ptr);
+	new_ptr = malloc(size);
+	ft_memcpy(new_ptr, ptr, size);
+	return (new_ptr);
+}
