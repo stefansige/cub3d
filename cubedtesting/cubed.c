68e8db04da4d4ac9d086327cc27cf95896f17d25
Stefan <stefko@LAPTOP-KR4631QR>
2024-04-11 02:38:58 +0200

    custom algorithm

--- cubedtesting/cubed.c
+++ cubedtesting/cubed.c
@@ -1,16 +1,12 @@
 #include <mlx.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <math.h>
 
 #define RED 0xFF0000
 #define GREEN 0x00FF00
 #define BLUE 0x0000FF
-
-
-typedef struct s_vars {
-    void *mlx;
-    void *win;
-}              t_vars;
 
 typedef struct s_point {
     int x;
@@ -19,31 +15,58 @@ typedef struct s_point {
 
 typedef struct s_player {
     t_point pos;
-    t_point dir;
+    int dir;
 }              t_player;
+
+typedef struct s_vars {
+    void *mlx;
+    void *win;
+    t_player player;
+}              t_vars;
 
 typedef struct s_ray {
     t_point pos;
-    t_point dir;
-    t_point ldir;
-    t_point rdir;
+    int dir;
 }              t_ray;
 
-int worldMap[7][7]=
+int worldMap[10][10]=
 {
-  {1,1,1,3,1,1,1},
-  {1,0,0,0,0,0,1},
-  {1,0,0,0,0,0,1},
-  {1,0,0,0,0,0,1},
-  {1,0,0,0,0,0,1},
-  {1,0,0,0,0,0,1},
-  {1,1,1,2,1,1,1}
+  {1,1,1,1,1,1,1,1,1,1},
+  {1,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,1},
+  {1,1,1,1,1,1,1,1,1,1}
 };
+
+int ft_fixangle(int angle)
+{
+    angle = (int)angle % 360;
+    if (angle < 0)
+        angle += 360;
+    return (angle);
+}
+
+double torad(int angle)
+{
+    return (angle * M_PI / 180);
+}
+
+int ft_abs(int n)
+{
+    if (n < 0)
+        return (-n);
+    return (n);
+}
 
 void draw_line(int x0, int y0, int x1, int y1, t_vars *vars, int color)
 {
-    int dx = abs(x1 - x0);
-    int dy = abs(y1 - y0);
+    int dx = ft_abs(x1 - x0);
+    int dy = ft_abs(y1 - y0);
     int sx = x0 < x1 ? 1 : -1;
     int sy = y0 < y1 ? 1 : -1;
     int err = (dx > dy ? dx : -dy) / 2;
@@ -68,6 +91,26 @@ void draw_line(int x0, int y0, int x1, int y1, t_vars *vars, int color)
     }
 }
 
+void ft_movepoint(int *x, int *y, int angle_degrees, int distance) {
+    // Ensure the angle is within the range of 0 to 360 degrees
+    angle_degrees %= 360;
+    if (angle_degrees < 0)
+        angle_degrees += 360;
+
+    // Convert angle from degrees to radians
+
+    double angle_radians = angle_degrees * M_PI / 180.0;
+
+    // Calculate new floating-point coordinates
+    double new_x = *x + distance * (cos(angle_radians) * 5);
+    double new_y = *y + distance * (sin(angle_radians) * 5);
+
+    // Round and update the integer coordinates
+    *x = (int)round(new_x);
+    *y = (int)round(new_y);
+}
+
+
 void fillsquare(int x0, int y0, int x1, int y1, t_vars *vars, int color)
 {
     for (int i = x0; i < x1; i++)
@@ -79,23 +122,131 @@ void fillsquare(int x0, int y0, int x1, int y1, t_vars *vars, int color)
     }
 }
 
+int ft_key(int keycode, t_vars *vars)
+{
+    if (keycode == 0xff1b)
+        exit(0);
+    if (keycode == 0xff53)
+        vars->player.dir += 10;
+    if (keycode == 0xff51)
+        vars->player.dir -= 10;
+    if (keycode == 0xff52)
+    {
+        ft_movepoint(&vars->player.pos.x, &vars->player.pos.y, vars->player.dir, 10);
+        printf("x: %d, y: %d\n", vars->player.pos.x, vars->player.pos.y);
+    }
+    if (keycode == 0xff54)
+    {
+        ft_movepoint(&vars->player.pos.x, &vars->player.pos.y, vars->player.dir + 180, 10);
+        printf("x: %d, y: %d\n", vars->player.pos.x, vars->player.pos.y);
+    }
+    return (0);
+}
+
+int ft_rays(t_vars *vars)
+{
+    t_ray ray;
+
+    for (int i = 0; i < 100; i++) // Iterate over 100 rays
+        {
+            // Calculate the ray direction based on the player's direction and field of view
+            int dir = vars->player.dir - 50 + i;
+
+            // Ensure the angle is within the range of 0 to 360 degrees
+            dir = ft_fixangle(dir);
+            ray.dir = dir;
+            ray.pos.x = vars->player.pos.x;
+            ray.pos.y = vars->player.pos.y;
+            int rx;
+            int ry;
+            double hipx;
+            double hipy;
+            if (ray.dir < 90)
+            {
+                rx = 100 - ray.pos.x % 100;
+                ry = 100 - ray.pos.y % 100;
+                    hipx = rx / cos(torad(ray.dir));
+                    hipy = ry / sin(torad(ray.dir));
+                    if (hipx <= hipy)
+                    {
+                        ray.pos.x += rx;
+                        ray.pos.y += hipx * sin(torad(ray.dir));
+                    }
+                    else
+                    {
+                        ray.pos.y += ry;
+                        ray.pos.x += hipy * cos(torad(ray.dir));
+                    }
+            }
+            else if (ray.dir < 180)
+            {
+                rx = ray.pos.x % 100;
+                ry = 100 - ray.pos.y % 100;
+                    hipy = rx / cos(torad(ray.dir - 90));
+                    hipx = ry / sin(torad(ray.dir - 90));
+                    if (hipx < hipy)
+                    {
+                        ray.pos.x -= rx;
+                        ray.pos.y += hipx * cos(torad(ray.dir - 90));
+                    }
+                    else
+                    {
+                        ray.pos.y += ry;
+                        ray.pos.x -= hipy * sin(torad(ray.dir - 90));
+                    }
+            }
+            else if (ray.dir < 270)
+            {
+                rx = ray.pos.x % 100;
+                ry = ray.pos.y % 100;
+                    hipx = rx / cos(torad(ray.dir - 180));
+                    hipy = ry / sin(torad(ray.dir - 180));
+                    if (hipx <= hipy)
+                    {
+                        ray.pos.x -= rx;
+                        ray.pos.y -= hipx * sin(torad(ray.dir - 180));
+                    }
+                    else
+                    {
+                        ray.pos.y -= ry;
+                        ray.pos.x -= hipy * cos(torad(ray.dir - 180));
+                    }
+            }
+            else
+            {
+                rx = 100 - ray.pos.x % 100;
+                ry = ray.pos.y % 100;
+                    hipy = rx / cos(torad(ray.dir - 270));
+                    hipx = ry / sin(torad(ray.dir - 270));
+                    if (hipx <= hipy)
+                    {
+                        ray.pos.x += rx;
+                        ray.pos.y -= hipx * cos(torad(ray.dir - 270));
+                    }
+                    else
+                    {
+                        ray.pos.y -= ry;
+                        ray.pos.x += hipy * sin(torad(ray.dir - 270));
+                    }
+            }
+            draw_line(vars->player.pos.x, vars->player.pos.y, ray.pos.x, ray.pos.y, vars, RED);
+        }
+
+    return (0);
+}
+
 int main(void)
 {
     t_vars vars;
-    t_player player;
 
-    player.pos.x = 350;
-    player.pos.y = 350;
-
-    player.dir.x = 50;
-    player.dir.y = 0;
-
-    t_ray ray;
+    vars.player.pos.x = 350;
+    vars.player.pos.y = 350;
+    vars.player.dir = 90;
 
     vars.mlx = mlx_init();
     vars.win = mlx_new_window(vars.mlx, 1000, 1000, "cuba");
-    draw_line(player.pos.x - 50, player.pos.y, player.pos.x + 50 , player.pos.y, &vars, GREEN);
-    draw_line(player.pos.x, player.pos.y - 50, player.pos.x, player.pos.y + 50, &vars, GREEN);
+/*   draw_line(vars.player.pos.x - 50, vars.player.pos.y, vars.player.pos.x + 50 , vars.player.pos.y, &vars, GREEN);
+    draw_line(vars.player.pos.x, vars.player.pos.y - 50, vars.player.pos.x, vars.player.pos.y + 50, &vars, GREEN);
     draw_line(100, 0, 100, 1000, &vars, BLUE);
     draw_line(200, 0, 200, 1000, &vars, BLUE);
     draw_line(300, 0, 300, 1000, &vars, BLUE);
@@ -118,11 +269,9 @@ int main(void)
     fillsquare(900, 0, 1000, 1000, &vars, BLUE);
     fillsquare(0, 0, 1000, 100, &vars, BLUE);
     fillsquare(0, 900, 1000, 1000, &vars, BLUE);
-
-    while (1)
-    {
-
-        mlx_loop(vars.mlx);
-    }
+    */
+    mlx_hook(vars.win, 2, 1L << 0, ft_key, &vars);
+    mlx_loop_hook(vars.mlx, ft_rays, &vars);
+    mlx_loop(vars.mlx);
     return (0);
 }
